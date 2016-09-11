a52891cfb1f0ebbe25ee54e28af57e678e2ee0d2
Luc Gommans <lgommans@users.noreply.github.com>
2016-09-11 03:32:39 +0300

    Add comment in config.php how to ignore config.php

--- www/config.php
+++ www/config.php
@@ -1,4 +1,5 @@
 <?php 
+
 // You need a MySQL database to connect to
 $db_host = 'p:localhost';
 $db_user = 'root';
@@ -20,3 +21,13 @@ $lock_timeout = $cache_duration; // seconds
 // The URL to contact (username and token parameters will be added).
 $factorio_url = 'https://multiplayer.factorio.com/get-games';
 
+/* Git instructions for developers (if you want to upload changes for the project):
+ * 
+ * Gitignore is a very nice thing which ignores you. That's why it's called gitignore: it ignores you.
+ * When you put www/config.php in gitignore, it will happily *not* ignore the config.php file and
+ * instead include it when you try to commit.
+ * Therefore, it is necessary to run this command if you want to exclude changes when contributing:
+ *  git update-index --assume-unchanged www/config.php
+ * Unlike a change in gitignore, this change is not included in a commit so we cannot solve it for
+ * once and for all. On git's behalf, I'm terribly sorry for the inconvenience.
+ */
